f867609cafac3dc734abc8082275dc0f35223d03
Sofia Caggiani <scaggiani@gmail.com>
2023-12-29 18:06:39 -0300

    Added new style for the expanded View Details

--- src/components/PatientCard.jsx
+++ src/components/PatientCard.jsx
@@ -1,6 +1,7 @@
-import { Card, Button, Container } from "react-bootstrap";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import EditPatientModal from "./EditPatientModal";
+import moment from "moment";
 
 function PatientCard({ patient }) {
   const [showMore, setShowMore] = useState(false);
@@ -14,39 +15,103 @@ function PatientCard({ patient }) {
   return (
     <div key={patient.id}>
       <Card className="border-0">
-        <Card.Img className="card-img-top" variant="top" src={patient.avatar} />
-        <Card.Body>
-          <Card.Title className="text-center">{patient.name}</Card.Title>
-          <Card.Text className="text-secondary">
-            <div className="text-center card-link">
-              <a
-                className="card-link-a"
-                href={patient.website}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {patient.website}
-              </a>
-            </div>
-            {showMore && <p>{patient.description}</p>}
-          </Card.Text>
-        </Card.Body>
-        <Container className="text-center">
-          <Button
-            className="card-btn border"
-            variant="light"
-            onClick={handleShowModal}
-          >
-            Edit
-          </Button>
-          <Button
-            className="card-btn border"
-            variant="light"
-            onClick={handleMoreClick}
-          >
-            {showMore ? "Hide" : "View"} details
-          </Button>
+        <Container>
+          <Row>
+            <Col xs="auto" className="text-center">
+              <Card.Img
+                className="card-img-top"
+                variant="top"
+                src={patient.avatar}
+              />
+              <Card.Body>
+                <Card.Title className="text-center">{patient.name}</Card.Title>
+                <Card.Text className="text-secondary">
+                  <div className="text-center card-link">
+                    <a
+                      className="card-link-a"
+                      href={patient.website}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {patient.website}
+                    </a>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+              <Container className="text-center">
+                <Button
+                  className="card-btn border"
+                  variant="light"
+                  onClick={handleShowModal}
+                >
+                  Edit
+                </Button>
+                <Button
+                  className="card-btn border"
+                  variant="light"
+                  onClick={handleMoreClick}
+                >
+                  {showMore ? "Hide" : "View"} details
+                </Button>
+              </Container>
+            </Col>
+            {showMore && (
+              <Col xs="8">
+                <Container>
+                  <Row className="border-bottom card-details">
+                    <Col>
+                      <Row>
+                        <Col>
+                          <div className="card-details-title">ID</div>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <div className="card-details-content">
+                            {patient.id}
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col>
+                      <Row>
+                        <Col>
+                          <div className="card-details-title">Created at</div>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <div className="card-details-content">
+                            {moment(patient.createdAt).format(
+                              "MMMM Do YYYY, h:mm:ss a"
+                            )}
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Row>
+                        <Col>
+                          <div className="card-details-title">Description</div>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <div className="card-details-content">
+                            {patient.description}
+                          </div>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Container>
+              </Col>
+            )}
+          </Row>
         </Container>
+
         <EditPatientModal
           patient={patient}
           show={show}
